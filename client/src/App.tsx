eda00b0b2cf37bf4c4b8a926919490e4cc6c015c
vignesh <madhanvic@gmail.com>
2026-01-01 21:10:16 +0530

    Dummy Commit for ssh

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,9 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
 import routes from "./routes/route";
 
-function App() {
+export default function App() {
   const router = createBrowserRouter(routes);
   return <RouterProvider router={router} />;
 }
 
-export default App;
+
