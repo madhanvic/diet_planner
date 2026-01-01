92b3d8050120d2ef056a3a9999d6d9d15cbff245
vignesh <madhanvic@gmail.com>
2026-01-01 21:20:05 +0530

    Dummy Message

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
 import routes from "./routes/route";
 
-export default function App() {
+function App() {
   const router = createBrowserRouter(routes);
   return <RouterProvider router={router} />;
 }
 
 
+export default App;
